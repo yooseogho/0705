2fba4840f4d0327ddc1c8a11d2ac75926cce78d2
유석호 <tjrgh6362@naver.com>
2023-07-05 14:42:24 +0900

    https http ajax설정

--- list.js
+++ list.js
@@ -14,15 +14,15 @@ return pageno;
 
 //기본 매개변수(default parameter)
 async function fetch(pageno=1,pagesize=10){
-  const api='http://sample.bmaster.kro.kr/contacts'
+  const api='http://sample.bmaster.kro.kr/contacts';
   const url=`${api}?pageno=${pageno}&pagesize=${pagesize}`;
   //$.ajax()는 병렬 처리 (비동기 처리)되는 코드->언제 끝날 지 모른다
   //비동기코드를 리턴받는 result는 "미래에 값이 들어올 것이다."란 값을 가진다
   //(Promise)
   try{
     const result= await $.ajax(url);
+    return result;
   }catch(err){
-    console.log(err)
     return null;
   }
 
